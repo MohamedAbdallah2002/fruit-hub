bb8c964c2b4fc19bae5faa123b0d5b5800261b8f
Mohamed Abdallah <mohamedelsebeay>
2025-04-01 10:43:53 +0200

    fruit_hub

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FruitHub());
+}
+class FruitHub extends StatelessWidget {
+  const FruitHub({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
