8a17c02044c64a3a05b5332ba210027b08e7b59b
Mohamed Abdallah <moelsebeay10@gmail.com>
2025-04-15 01:20:13 +0200

    splash view done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const FruitHub());
@@ -10,7 +11,7 @@ class FruitHub extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: SplashView(),
     );
   }
 }
